94799cf93f4965107ee0c2b6b5a99d43c0552fb9
Aditya Vardhan M S <111335503+aditya180604@users.noreply.github.com>
2023-02-03 16:58:54 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+data = pd.read_csv("/data.csv");
+print(data," ")
+print("-------------------------------------------------------------------
+---")
+d=np.array(data)[:,:-1]
+print("The attributes are :",d)
+print("-------------------------------------------------------------------
+---")
+target = np.array(data)[:,-1]
+print("\n The target is: ",target)
+def train(c,t):
+ for i, val in enumerate(t):
+ if val == "Yes":
+ specific_hypothesis = c[i].copy()
+ break
+ 
+ for i, val in enumerate(c):
+ if t[i] == "Yes":
+ for x in range(len(specific_hypothesis)):
+ if val[x] != specific_hypothesis[x]:
+ specific_hypothesis[x] = '?'
+ else:
+ pass
+ 
+ return specific_hypothesis
+print("\n The final hypothesis is:",train(d,target)
